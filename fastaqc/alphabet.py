a3eb946e92f434d005b3f13b0e7fe5a8bcfc7c71
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2020-08-05 14:03:52 +0200

    Implement special character counts per category

---
+++ fastaqc/alphabet.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+# source https://www.ddbj.nig.ac.jp/ddbj/code-e.html
+_dna_unamb_chars = ['A','T','G','C']
+_rna_unamb_chars = ['A','U','G','C']
+_aa_unamb_chars = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
+_dna_amb_chars = ['N','R','Y','S','W','K','M','B','D','H','V']
+_rna_amb_chars = _dna_unamb_chars
+_aa_amb_chars = ['X','B','Z','J']
+_dna_special_chars = ['-'] # gap
+_rna_special_chars = _dna_special_chars
+_aa_special_chars = ['*', '-'] # stop and gap
+
+class Alphabet(Enum):
+  DNA = (_dna_unamb_chars, _dna_amb_chars, _dna_special_chars)
+  RNA = (_rna_unamb_chars, _rna_amb_chars, _rna_special_chars)
+  AA = (_aa_unamb_chars, _aa_amb_chars, _aa_special_chars )
+  # other contains all characters from above, but everything from the ambiguous section
+  # is added to the non-ambiguous section
+  OTHER = (
+    list(set(_dna_unamb_chars + _rna_unamb_chars + _aa_unamb_chars + _dna_amb_chars + _rna_amb_chars )),
+    _aa_amb_chars,
+    list(set(_dna_special_chars + _rna_special_chars + _aa_special_chars))
+    )
+
+  def __init__(self, unambiguous_chars, ambiguous_chars, special_chars):
+    self.unambiguous_chars = unambiguous_chars
+    self.ambiguous_chars = ambiguous_chars
+    self.special_chars = special_chars
+    self.all_chars = unambiguous_chars + ambiguous_chars + special_chars
--- fastaqc/main.py
+++ fastaqc/main.py
@@ -7,12 +7,15 @@ import itertools
 from prettytable import PrettyTable
 import tabulate
 from xopen import xopen
+from fastaqc.alphabet import Alphabet
+import pprint
 
 __version__ = VersionInfo('fastaqc').semantic_version().release_string()
 
 def main():
     parser = argparse.ArgumentParser(description='Version ' + __version__ + '\nValidate fasta file', formatter_class=RawTextHelpFormatter)
     parser.set_defaults(func=help)
+    parser.add_argument('--verbose', '-v', action="store_true")
 
     subparsers = parser.add_subparsers()
     info_parser = subparsers.add_parser('info')
@@ -31,15 +34,29 @@ def help(args, config):
 
 def info(args, cfg):
   from Bio import SeqIO
+
+  # Concept
+  #
+  # The base method iterates through all sequences in the fasta file and applies multiple 
+  # actions on it. Each action is modelled as a function with two parameters, the sequence
+  # record and the stats-dictionary. The stats-dictionary holds the results of each action.
+  # Subsequent actions can access the results from previous actions.
+  #
+  # Coonvetions for stats dictionary
+  #  * Intermediary results for only one sequence (those that will not be presented to the user)
+  #    start with an underscore.
   checks = [
     count, 
     compute_character_distribution,
-    check_sequence_type,
-    check_has_star,
-    check_has_dot,
-    check_has_X,
-    check_has_N,
-    check_has_dash,
+    compute_character_positions,
+    detect_sequence_type,
+    detect_ambiguous_and_special_characters,
+    set_sequence_category_name,
+    count_sequence_types,
+    count_sequences_with_special_characters,
+    count_sequences_with_ambiguous_characters,
+    count_sequences_with_unknown_characters,
+    clear_tempary_fields
   ]
 
   for filename in args.fasta:
@@ -52,73 +69,38 @@ def info(args, cfg):
           c(record, stats)
       print_stats(stats)
 
+def clear_tempary_fields(record, stats):
+  for_removal = []
+  for k in stats.keys():
+    if k.startswith('_'):
+      for_removal.append(k)
+
+  for k in for_removal:
+    del stats[k]
+
 def print_stats(stats):
   header = [stats['filename'], 'count']
   table = []
   table.append(['sequences', stats['sequences']])
-  _add_row_if_non_null2(table, stats['sequence_types'], 'aminoacid_unambiguous', '   AA')
-  _add_row_if_non_null2(table, stats['sequence_types'], 'aminoacid_ambiguous', '   AA (ambiguous)')
-  _add_row_if_non_null2(table, stats['sequence_types'], 'dna_unambiguous', '   DNA')
-  _add_row_if_non_null2(table, stats['sequence_types'], 'dna_ambiguous', '    DNA (ambiguous)')
-  _add_row_if_non_null2(table, stats['sequence_types'], 'rna_ambiguous', '   RNA')
-  _add_row_if_non_null2(table, stats['sequence_types'], 'rna_unambiguous', '   RNA (ambiguous)')
-  _add_row_if_non_null2(table, stats['sequence_types'], 'other', '   other')
-  table.append(['',''])
-  table.append(['sequences with special characters',''])
-  _add_row_if_non_null2(table, stats, 'Xs', '   X')
-  _add_row_if_non_null2(table, stats, 'Ns', '   N')
-  _add_row_if_non_null2(table, stats, 'stars', '   *')
-  _add_row_if_non_null2(table, stats, 'dots', '   .')
-  _add_row_if_non_null2(table, stats, 'dash', '   -')
-  _add_row_if_non_null2(table, stats, 'unhandled_characters', 'unknown characters')
+  logging.info(pprint.pformat(stats))
+  for k,v in sorted(stats['type_counts'].items()):
+    table.append(['   ' + k, v])
+    if 'special_char_count' in stats:
+      if k in stats['special_char_count']:
+        for k2,v2 in sorted(stats['special_char_count'][k].items()):
+          table.append(['      ' + k2,v2])
+    if 'ambiguous_char_count' in stats:
+      if k in stats['ambiguous_char_count']:
+        for k2,v2 in sorted(stats['ambiguous_char_count'][k].items()):
+          table.append(['      ' + k2,v2])
+    if 'unknown_char_count' in stats:
+      if k in stats['unknown_char_count']:
+        for k2,v2 in sorted(stats['unknown_char_count'][k].items()):
+          table.append(['      ' + k2,v2])
+
   tabulate.PRESERVE_WHITESPACE = True
-  print(tabulate.tabulate(table, headers=header, tablefmt='fancy_grid', colalign=('left', 'right')))
+  print(tabulate.tabulate(table, headers=header, tablefmt='pretty', colalign=('left', 'right')))
   print('')
-
-def _add_row_if_non_null2(table, stats, field, fieldname=None):
-  if field in stats:
-    if stats[field]:
-      if fieldname:
-        table.append([fieldname, stats[field]])
-      else:
-        table.append([field, stats[field]])
-
-def print_stats_with_PTable(stats):
-  table = PrettyTable(['field', 'count'])
-  table.align['field'] = 'l'
-  table.align['count'] = 'r'
-  table.add_row(['total sequences', stats['sequences']])
-  _add_row_if_non_null(table, stats['sequence_types'], 'aminoacid_unambiguous', 'AA')
-  _add_row_if_non_null(table, stats['sequence_types'], 'aminoacid_ambiguous', 'AA with X')
-  _add_row_if_non_null(table, stats['sequence_types'], 'dna_unambiguous', 'DNA')
-  _add_row_if_non_null(table, stats['sequence_types'], 'dna_ambiguous', 'DNA with N, ...')
-  _add_row_if_non_null(table, stats['sequence_types'], 'rna_ambiguous', 'RNA')
-  _add_row_if_non_null(table, stats['sequence_types'], 'rna_unambiguous', 'RNA with N, ...')
-  _add_row_if_non_null(table, stats['sequence_types'], 'other')
-  _add_row_if_non_null(table, stats, 'Xs', '# sequences containing X')
-  _add_row_if_non_null(table, stats, 'Ns', '# sequences containing N')
-  _add_row_if_non_null(table, stats, 'stars', '# sequences containing *')
-  _add_row_if_non_null(table, stats, 'dots', '# sequences containing .')
-  _add_row_if_non_null(table, stats, 'dash', '# sequences containing -')
-  _add_row_if_non_null(table, stats, 'unhandled_characters', 'unknown characters')
-
-  print(table.get_string(title=stats['filename']))
-  print('')
-
-
-def _add_row_if_non_null(table, stats, field, fieldname=None):
-  if field in stats:
-    if stats[field]:
-      if fieldname:
-        table.add_row([fieldname, stats[field]])
-      else:
-        table.add_row([field, stats[field]])
-
-
-def _print_if_non_null(template, stats, field):
-  if field in stats:
-    if stats[field] :
-      print(template.format(stats[field]))
 
 def count(record, stats):
   if 'sequences' not in stats:
@@ -130,13 +112,55 @@ def compute_character_distribution(record, stats):
   for c in record.seq.upper():
     if c not in distribution:
       distribution[c] = 0
-    else:
-      distribution[c] = distribution[c] + 1
-  stats['character_distribution'] = distribution
+    distribution[c] = distribution[c] + 1
 
-def check_is_protein(record, stats):
-  for c in record.seq:
-    protein_alphabet = {''}
+  stats['_character_distribution'] = distribution
+
+def compute_character_positions(record, stats):
+  positions = {}
+  for i, c in enumerate(record.seq.upper()):
+    if c not in positions:
+      positions[c] = []
+    positions[c].append(i)
+  stats['_character_positions'] = positions
+
+def count_sequences_with_special_characters(record, stats):
+  alphabet = assert_sequence_type_available(stats)
+  _count(stats, 'special_char_count', alphabet.special_chars)
+
+def count_sequences_with_ambiguous_characters(record, stats):
+  alphabet = assert_sequence_type_available(stats)
+  _count(stats, 'ambiguous_char_count', alphabet.ambiguous_chars)
+
+def _count(stats, fieldname, chars):
+  category_name = assert_sequence_category_name_available(stats)
+  c_dist = assert_character_distribution_available(stats)
+  if fieldname not in stats:
+    stats[fieldname] = {}
+  if category_name not in stats[fieldname]:
+    stats[fieldname][category_name] = {}
+  counts = stats[fieldname][category_name]
+  for c in chars:
+    if c in c_dist:
+      if c not in counts:
+        counts[c] = 0
+      counts[c] = counts[c] + 1
+
+def count_sequences_with_unknown_characters(record, stats):
+  category_name = assert_sequence_category_name_available(stats)
+  alphabet = assert_sequence_type_available(stats)
+  c_dist = assert_character_distribution_available(stats)
+  if 'unknown_char_count' not in stats:
+    stats['unknown_char_count'] = {}
+  if category_name not in stats['unknown_char_count']:
+    stats['unknown_char_count'][category_name] = {}
+  counts = stats['unknown_char_count'][category_name]
+  chars = set(alphabet.all_chars)
+  for c in c_dist.keys():
+    if c not in chars:
+      if c not in counts:
+        counts[c] = 0
+      counts[c] = counts[c] + 1
 
 def check_has_X(record, stats):
   '''Checks if the sequence contains 'X'. This character denotes ambiguity in
@@ -165,71 +189,60 @@ def check_has_N(record, stats):
   DNA sequences.'''
   _process_character(stats, 'N', 'Ns')
 
-dna_unambiguous_alphabet = ['A','T','G','C']
+dna_unambiguous_alphabet = Alphabet.DNA.unambiguous_chars
 dna_ambiguous_alphabet = ['A','T','G','C','N','R','Y','S','W','K','M','B','D','H','V','.','-']
 rna_unambiguous_alphabet = ['A','U','G','C']
 rna_ambiguous_alphabet = ['A','U','G','C','N','R','Y','S','W','K','M','B','D','H','V','.','-']
 aa_unambiguous_alphabet=['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y','*']
-# source https://www.ddbj.nig.ac.jp/ddbj/code-e.html
 aa_ambiguous_alphabet=['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y','X','*','_','B','Z','J']
 
 allchars = set(dna_unambiguous_alphabet + dna_ambiguous_alphabet + rna_unambiguous_alphabet + rna_ambiguous_alphabet + aa_unambiguous_alphabet + aa_ambiguous_alphabet)
 
-##############
-# is this really needed?
-
-alphabets = {
-  'dna_unambiguous': dna_unambiguous_alphabet,
-  'dna_ambiguous': dna_ambiguous_alphabet,
-  'rna_unambiguous': rna_unambiguous_alphabet,
-  'rna_ambiguous': rna_ambiguous_alphabet,
-  'aa_unambiguous': aa_unambiguous_alphabet,
-  'aa_ambiguous': aa_ambiguous_alphabet,
-}
-################
-
-def check_sequence_type(record, stats):
-  c_dist = stats['character_distribution']
-  if not 'sequence_types' in stats:
-    stats['sequence_types'] = {
-      'dna_unambiguous': 0,
-      'rna_unambiguous': 0,
-      'dna_ambiguous': 0,
-      'rna_ambiguous': 0,
-      'aminoacid_unambiguous': 0,
-      'aminoacid_ambiguous': 0,
-      'other': 0,
-    }
-  sequence_types = stats['sequence_types']
-    
-  if not 'unhandled_characters' in stats:
-    stats['unhandled_characters'] = {
-    }
-  unhandled_characters = stats['unhandled_characters']
-
-  if contains_only(c_dist, dna_unambiguous_alphabet):
-    sequence_types['dna_unambiguous'] = sequence_types['dna_unambiguous'] + 1
-  elif contains_only(c_dist, rna_unambiguous_alphabet):
-    sequence_types['rna_unambiguous'] = sequence_types['rna_unambiguous'] + 1
-  elif contains_only(c_dist, dna_ambiguous_alphabet):
-    sequence_types['dna_ambiguous'] = sequence_types['dna_ambiguous'] + 1
-  elif contains_only(c_dist, rna_ambiguous_alphabet):
-    sequence_types['rna_ambiguous'] = sequence_types['rna_ambiguous'] + 1
-  elif contains_only(c_dist, aa_unambiguous_alphabet):
-    sequence_types['aminoacid_unambiguous'] = sequence_types['aminoacid_unambiguous'] + 1
-  elif contains_only(c_dist, aa_ambiguous_alphabet):
-    sequence_types['aminoacid_ambiguous'] = sequence_types['aminoacid_ambiguous'] + 1
+def detect_sequence_type(record, stats):
+  c_dist = assert_character_distribution_available(stats)
+  if contains_only(c_dist, Alphabet.DNA.all_chars):
+    stats['_type'] = Alphabet.DNA
+  elif contains_only(c_dist, Alphabet.RNA.all_chars):
+    stats['_type'] = Alphabet.RNA
+  elif contains_only(c_dist, Alphabet.AA.all_chars):
+    stats['_type'] = Alphabet.AA
   else:
-    sequence_types['other'] = sequence_types['other'] + 1
-    identify_unknown_character(unhandled_characters, c_dist)
+    stats['_type'] = Alphabet.OTHER
 
-def identify_unknown_character(unhandled_characters, character_distribution):
-  characters = character_distribution.keys()
+def detect_ambiguous_and_special_characters(record, stats):
+  c_dist = assert_character_distribution_available(stats)
+  alphabet = assert_sequence_type_available(stats)
+  type_flags = set()
+  if contains_only(c_dist, alphabet.unambiguous_chars):
+    type_flags.add('unambiguous')
+  else:
+    if contains(c_dist, alphabet.ambiguous_chars):
+      type_flags.add('ambiguous')
+    if contains(c_dist, alphabet.special_chars):
+      type_flags.add('special')
+  stats['_type_flags'] = type_flags
+
+def set_sequence_category_name(record, stats):
+  alphabet = assert_sequence_type_available(stats)
+  flags = assert_sequence_type_flags_available(stats)
+  name = "{} ({})".format(alphabet.name, ",".join(sorted(flags)))
+  stats['_category_name'] = name
+
+def count_sequence_types(record, stats):
+  alphabet = assert_sequence_type_available(stats)
+  flags = assert_sequence_type_flags_available(stats)
+  type = assert_sequence_category_name_available(stats)
+  if 'type_counts' not in stats:
+    stats['type_counts'] = {}
+  if type not in stats['type_counts']:
+    stats['type_counts'][type] = 0
+  stats['type_counts'][type] = stats['type_counts'][type]  + 1
+
+def contains(dist, characters):
   for c in characters:
-    if c not in allchars:
-      if c not in unhandled_characters:
-        unhandled_characters[c] = 0
-      unhandled_characters[c] = unhandled_characters[c] + 1
+    if c in dist:
+      return True
+  return False
 
 def contains_only(dist, characters):
   remaining = len(dist)
@@ -239,10 +252,27 @@ def contains_only(dist, characters):
   return remaining == 0
 
 def _process_character(stats, character, field):
-  if character in stats['character_distribution']:
+  assert_character_distribution_available(stats)
+  if character in stats['_character_distribution']:
     if field not in stats:
       stats[field] = 0
     stats[field] = stats[field] + 1
+
+def assert_character_distribution_available(stats):
+  assert '_character_distribution' in stats, 'Sequence character distribution not availabe. It must be computed before this check.'
+  return stats['_character_distribution']
+
+def assert_sequence_type_available(stats):
+  assert '_type' in stats, 'Sequence type information not availabe. It must be computed before this check.'
+  return stats['_type']
+
+def assert_sequence_type_flags_available(stats):
+  assert '_type_flags' in stats, 'Sequence type flag information not availabe. It must be computed before this check.'
+  return stats['_type_flags']
+
+def assert_sequence_category_name_available(stats):
+  assert '_category_name' in stats, 'Sequence category_name information not availabe. It must be computed before this check.'
+  return stats['_category_name']
 
 if __name__ == "__main__":
     main()
